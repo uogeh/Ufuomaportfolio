87f04e421f080482fdda6d5d5bdecee79d3cc4a8
Ufuoma Ogeh <44295740+uogeh@users.noreply.github.com>
2025-05-16 01:25:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // Add interactivity if needed
 console.log("Website loaded successfully.");
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
